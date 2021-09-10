d821d1593c5debdce127a688ce8336e8704d0596
DamarjatiRahmandaru <damarjatir11@gmail.com>
2021-09-10 18:38:28 +0700

    up
    up

--- tugas-modul-2-kelompok-40/src/index.js
+++ tugas-modul-2-kelompok-40/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 import Firsta from "./components/Firsta"
 import data from "./assets/dummy/data.json"
+import Damar from "./components/damar"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +15,14 @@ ReactDOM.render(
       name="Firsta Adi Pradana"
       date="9/9/2021"
       />
+    <Damar
+      ucapan="Selamat Berbelanja!"
+      jenis="CATEGORIES"
+      buah="Fruits"
+      sayur="Vegetables"
+      minuman="Drinks"
+      jajan="Snacks"
+      />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
