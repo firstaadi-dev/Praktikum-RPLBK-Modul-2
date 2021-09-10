5fda77ad4a63a86b9dac103929d40505ea44ad3f
Firsta Adi Pradana <firstaadi@hotmail.com>
2021-09-10 18:46:10 +0700

    change title color

--- tugas-modul-2-kelompok-40/src/App.js
+++ tugas-modul-2-kelompok-40/src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="home">
-      <div className="flex items-center h-16 justify-center bg-indigo-200"
+      <div className="flex items-center h-16 justify-center bg-gray-700"
            >
-        <h1 className="font-extrabold text-4xl">
+        <h1 className="font-bold text-4xl text-gray-100">
           <span className="block">Tugas Kelompok 40 Modul 2</span>
         </h1>
       </div>
